1e350a81601735d35f91b30af88d10ecb6d6dd95
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-07-16 13:42:48 +0500

    feat: add Head component with metadata for Clients, Invoice, Dashboard, Project, and Settings pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,23 @@
 "user-client"
+import Head from "next/head"
 import Nav from "./components/Nav"
 
 
 function page() {
   return (
+    <>
+    <Head>
+      <title>DashBoard | Freelance Flow</title>
+      <meta name="description" content="View and manage all your freelance clients, projects and invoices easily in one place with Freelance Flow." />
+      <meta name="keywords" content="freelancer, client management, freelance dashboard, Freelance Flow" />
+    </Head>
     <div className="lg:ml-[20vw] sm:ml-[25vw] px-5 py-2 lg:w-[80vw] sm:w-[75vw] h-screen">
       <Nav/>
       <div className="w-full h-screen mt-4 rounded-2xl shadow-xl bg-[#F7F7F7]">
         
       </div>
     </div>
+    </>
   )
 }
 
